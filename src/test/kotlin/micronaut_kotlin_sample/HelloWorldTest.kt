32da47917929db54968e090a4930b03f18e1ea65
harutaka <10650294+harutaka@users.noreply.github.com>
2025-04-11 08:06:21 +0000

    テスト作成

---
+++ src/test/kotlin/micronaut_kotlin_sample/HelloWorldTest.kt
@@ -0,0 +1,33 @@
+package micronaut_kotlin_sample
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import io.restassured.specification.RequestSpecification
+import org.junit.jupiter.api.Test
+
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.CoreMatchers.`is`
+
+@MicronautTest
+class HelloWorldTest {
+    @Test
+    fun testGetTodo(spec: RequestSpecification) {
+        spec
+            .`when`()
+                .get("/hello")
+            .then()
+                .statusCode(200)
+                .body(notNullValue())
+    }
+
+    @Test
+    fun testCreateGreeting(spec: RequestSpecification) {
+        spec
+            .given()
+                .param("name", "John")
+            .`when`()
+                .post("/hello")
+            .then()
+                .statusCode(200)
+                .body(`is`("Hello John"))
+    }
+}
