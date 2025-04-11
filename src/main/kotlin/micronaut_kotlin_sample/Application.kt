9e27351728980c6b24d2758d9c8db3baa26891eb
harutaka <10650294+harutaka@users.noreply.github.com>
2025-04-11 03:57:29 +0000

    first commit

---
+++ src/main/kotlin/micronaut_kotlin_sample/Application.kt
@@ -0,0 +1,7 @@
+package micronaut_kotlin_sample
+
+import io.micronaut.runtime.Micronaut.run
+fun main(args: Array<String>) {
+	run(*args)
+}
+
---
+++ src/test/kotlin/micronaut_kotlin_sample/Micronaut_kotlin_sampleTest.kt
@@ -0,0 +1,19 @@
+package micronaut_kotlin_sample
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class Micronaut_kotlin_sampleTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
