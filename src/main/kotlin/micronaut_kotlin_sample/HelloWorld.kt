7ecbd9261e00772da76edfd824c494844553852b
harutaka <10650294+harutaka@users.noreply.github.com>
2025-04-11 06:29:27 +0000

    HTTPクライアント機能追加

--- src/main/kotlin/micronaut_kotlin_sample/HelloWorld.kt
+++ src/main/kotlin/micronaut_kotlin_sample/HelloWorld.kt
@@ -5,14 +5,37 @@ import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.annotation.Body
 import io.micronaut.serde.annotation.Serdeable
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.HttpRequest
+import io.micronaut.scheduling.TaskExecutors
+import io.micronaut.scheduling.annotation.ExecuteOn
+import jakarta.inject.Inject
 
 @Controller("/hello")
+@ExecuteOn(TaskExecutors.BLOCKING)
 class HelloWorld {
+    @Inject
+    @field:Client("https://jsonplaceholder.typicode.com")
+    lateinit var httpClient: HttpClient
+    
     @Serdeable
     data class RequestBody(val name: String)
+    
+    @Serdeable
+    data class TodoResponse(val userId: Int, val id: Int, val title: String, val completed: Boolean)
+    
+    @Serdeable
+    data class CustomResponse(val id: Int, val title: String)
 
     @Get
-    fun get() = "Hello World"
+    fun get(): CustomResponse {
+        val response = httpClient.toBlocking().retrieve(
+            HttpRequest.GET<Any>("/todos/1"),
+            TodoResponse::class.java
+        )
+        return CustomResponse(response.id, response.title)
+    }
 
     @Post
     fun post(@Body body: RequestBody): String {
